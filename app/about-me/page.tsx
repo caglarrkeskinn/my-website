1b81f4618d676ccaca968ccad9d503329e3352f3
Çağlar Keskin <caglar@Caglar-MacBook-Pro.local>
2024-06-01 00:14:14 +0300

    UI changes

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -9,10 +9,10 @@ export default function AboutMe() {
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-5xl font-bold text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 ">
                 <Hero />
-                <Link href="/CaglarKeskinCV.pdf" className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 decoration-sky-500 underline underline-offset-[12px]" target='_blank'>
-                <FaLink className="inline-block mr-2 text-sky-500 w-7 " />
+                <Link href="/CaglarKeskinCV.pdf" className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 decoration-sky-500 underline hover:underline-offset-[12px]" target='_blank'>
+
                     View My CV 
-                <FaLink className="inline-block ml-2 text-sky-500 w-7 rotate-90" />
+
                 </Link>
             </h1>
         </div>
