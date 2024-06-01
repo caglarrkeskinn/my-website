0fc88bf0a2164ab43d9d30d6219f4991c3103821
Çağlar Keskin <caglar@Caglar-MBP.home>
2024-06-01 03:01:19 +0300

    webm video for mobile

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -8,6 +8,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
+        playsInline
         className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
       >
         <source src="/blackhole.webm" type="video/webm" />
