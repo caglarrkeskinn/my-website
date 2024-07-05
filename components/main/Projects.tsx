f47e9721058b17673b903f8c0a663f96209d634a
Çağlar Keskin <caglar@Caglar-MacBook-Pro.local>
2024-07-06 00:49:50 +0300

    Projects page header

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <div className="flex flex-col items-center justify-center py-10 md:py-20 z-[1]" id="projects">
       <h1 className="text-[32px] md:text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10 md:py-20 text-shadow-custom">
-        My Projects
+        Small Projects
       </h1>
       <div className="w-full flex flex-col lg:flex-row gap-10 px-5 md:px-10">
         
